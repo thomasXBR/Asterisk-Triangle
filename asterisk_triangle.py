c1a915fa715541d159e1c516154891ca806fea51
Thomas <thomas.bulhoes@gmail.com>
2025-04-16 13:39:59 -0300

    Fix: -"Negative height spacing"

--- asterisk_triangle.py
+++ asterisk_triangle.py
@@ -9,11 +9,13 @@ while True:
             for c in range(1, height*2, 2):
                 print(space*(space_count_pos), '*'*c)
                 space_count_pos-=1
+            print(" ")
     elif height < 0:
         while space_count_neg != height*(-1):
             for c in range((height*(-1))*2-1, 0, -2):
-                print(space*(space_count_neg), '*'*c)
+                print(space+' '*(space_count_neg), '*'*c)
                 space_count_neg+=1
+            print(" ")
 
     else:
         print("End")
